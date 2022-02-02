900808facbf11da2dcdce7ed730d20a682ce8d5d
Anjali Rawat <anjalirawat0807@gmail.com>
2022-02-03 02:22:21 +0530

    login Commit

---
+++ app/src/main/java/com/example/ideapitchingapp/AddIdeaActivity.java
@@ -0,0 +1,14 @@
+package com.example.ideapitchingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddIdeaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_idea);
+    }
+}
